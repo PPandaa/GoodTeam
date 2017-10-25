9b52727e4f891440d8500212c4fbebe3164e7c8d
jerry80088 <33078522+jerry80088@users.noreply.github.com>
2017-10-25 15:36:39 +0800

    Update test.java

--- test.java
+++ test.java
@@ -2,6 +2,8 @@ public class test
 {
 	public static void main(String args[])
 	{
-		System.out.println("");
+		System.out.println("我恨你");
+		System.out.println("I hate you");
+		System.out.println("Senden nefret ediyorum");
 	}
 }
